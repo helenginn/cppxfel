5a82084d2c52f8d337ddd661cb4130f16d7a41c1
Helen Ginn <ginn@rescomp2.(none)>
2016-03-21 15:57:23 +0000

    Fixing bugs for LCLS - CXI_extract

--- command_line/cxi_extract.py
+++ command_line/cxi_extract.py
@@ -19,6 +19,7 @@ width = 1765
 height = 1765
 skip = 0
 sacla = False
+max = 0
 
 print "CXI to pickle dumper"
 
@@ -46,6 +47,8 @@ for arg in sys.argv[1:]:
 		skip = int(key_value[1])
 	elif (key_value[0] == "sacla"):
 		sacla = (key_value[1] == "True")
+	elif (key_value[0] == "max"):
+		max = int(key_value[1])
 	else:
 		print "Did not understand", arg
 
@@ -69,77 +72,133 @@ if not sacla:
 	entries.append(entry + "/data_1/data")
 else:
 	for key in cxi.keys():
-		entries.append(key + "/data")
+		entries.append(key)
 
-for anEntry in entries:
-	num_images = cxi[anEntry].len()
+print "All keys:"
+print entries
 
-	print "Examining geometry file: finding rigid_group_d0."
-	panels = []
-	geomfile = open(geometry, 'r')
-	beginning = geomfile.tell()
+rigidGroupString = "rigid_group_d0"
+if sacla:
+	rigidGroupString = "rigid_group_collection_connected"
 
+print "Examining geometry file: finding", rigidGroupString
+
+panels = []
+geomfile = open(geometry, 'r')
+beginning = geomfile.tell()
+
+for line in geomfile:
+	if line[0] == ";":
+		continue
+	components = line.split("=")
+	if (components < 2):
+		continue
+	if components[0].strip() == rigidGroupString:
+		print "Found", rigidGroupString
+		panels = components[1].strip().split(",")
+		print "This group has", len(panels), "panels."
+		break
+
+panelInfo = {}
+
+for panel in panels:
+	geomfile.seek(beginning)
+	panelInfo[panel] = {}
 	for line in geomfile:
-		if line[0] == ";":
-			continue
-		components = line.split("=")
-		if (components < 2):
-			continue
-		if components[0].strip() == "rigid_group_d0":
-			print "Found rigid_group_d0"
-			panels = components[1].strip().split(",")
-			print "This group has", len(panels), "panels."
-			break
+		if (line[:len(panel)] == panel and line[len(panel):len(panel)+1] == "/"):
+			key_value = line.split("=")
+			key = key_value[0].strip()
+			value = key_value[1].strip()
+		
+			id_property = key.split("/")
+			id = id_property[0]
+			property = id_property[1]
+		
+			if len(value.split(" ")) > 1:
+				subProperties = value.split(" ")
+				for i in range(len(subProperties)):
+					subProperties[i] = subProperties[i].replace("x", "").replace("y", "")
+					subProperties[i] = float(subProperties[i])
+				value = subProperties
+			else:
+				value = float(value)
+		
+			print "Found value", value, "for panel", panel, ", property", property
+			panelInfo[panel][property] = value
 
-	panelInfo = {}
+geomfile.close()
 
-	for panel in panels:
-		geomfile.seek(beginning)
-		panelInfo[panel] = {}
-		for line in geomfile:
-			if (line[:len(panel)] == panel and line[len(panel):len(panel)+1] == "/"):
-				key_value = line.split("=")
-				key = key_value[0].strip()
-				value = key_value[1].strip()
-			
-				id_property = key.split("/")
-				id = id_property[0]
-				property = id_property[1]
-			
-				if len(value.split(" ")) > 1:
-					subProperties = value.split(" ")
-					for i in range(len(subProperties)):
-						subProperties[i] = subProperties[i].replace("x", "").replace("y", "")
-						subProperties[i] = float(subProperties[i])
-					value = subProperties
-				else:
-					value = float(value)
-			
-				print "Found value", value, "for panel", panel, ", property", property
-				panelInfo[panel][property] = value
+realMin = skip
+realMax = max + skip
 
-	geomfile.close()
+if max == 0:
+	realMax = len(entries)
+
+if (skip + max) > len(entries):
+	realMax = len(entries)
+
+if not sacla:
+	realMin = 0
+	realMax = 1
+
+for anEntry in entries[realMin:realMax]:
+	print anEntry
+	dataEntry = anEntry
+	
+	if (sacla):
+		dataEntry = anEntry + "/data"
+	
+	num_images = cxi[dataEntry].len()
+
+	if sacla:
+		num_images = 1
 
 	print "\nEntry has", num_images, "images.\n"
 	sample_file = open('sample.pickle', 'rb')
 	sample = pickle.load(sample_file)
 
-	for i in range(skip, num_images):
-		image = cxi[entry + "/data_1/data"][i]
-		identifier = cxi[entry + "/data_1/experiment_identifier"][i]
-		distance = cxi[entry + "/data_1/distance"][i] * 1000
-		pixelSize = cxi[entry + "/data_1/x_pixel_size"][i] * 1000
-		energy = cxi[entry + "/instrument_1/source_1/energy"][i]
-		keV = energy * 6.2416006565e+15
-		wavelength = 12.4 / keV
+	realEntryMin = 0
+	realEntryMax = 1
+	
+	if (max == 0):
+		max = num_images
+	
+	if not sacla:
+		realEntryMin = skip
+		realEntryMax = skip + max
+		
+		if (skip + max) > num_images:
+			realEntryMax = num_images
+
+	for i in range(realEntryMin, realEntryMax):
+		image = None
+		identifier = anEntry
+		distance = 50
+		pixelSize = 0.05
+		wavelength = None
+		if not sacla:
+			image = cxi[entry + "/data_1/data"][i]
+			identifier = cxi[entry + "/data_1/experiment_identifier"][i]
+			distance = cxi[entry + "/data_1/distance"][i] * 1000
+			pixelSize = cxi[entry + "/data_1/x_pixel_size"][i] * 1000
+			energy = cxi[entry + "/instrument_1/source_1/energy"][i]
+			keV = energy * 6.2416006565e+15
+			wavelength = 12.4 / keV
+		else:
+			image = cxi[anEntry + "/data"]
+			wavelength = cxi[anEntry + "/photon_wavelength_A"].value
+		
 		print "Dumping", identifier
 		alldata = []
 		count = 0
 		rowSize = len(image[0])
-		for row in image:
-			rowdata = numpy.array(row, dtype=numpy.int32)
-			alldata.extend(row)
-			count += 1
+		if sacla:
+			alldata = numpy.concatenate(image[:])
+		else:
+			for row in image:
+				rowdata = numpy.array(row, dtype=numpy.int32)
+				alldata.extend(row)
+				count += 1
 		totalPicklePixels = width * height
 		print "Generating canvas for pickle image with", totalPicklePixels, "pixels."
 
@@ -180,14 +239,14 @@ for anEntry in entries:
 		sample['DISTANCE'] = distance
 		sample['SIZE1'] = width
 		sample['SIZE2'] = height
-		sample['ACTIVE_AREAS'] = scitbx_array_family_flex_ext.int([0, 0, width, height])
-		sample['PIXEL_SIZE'] = pixelSize
-		sample['BEAM_CENTER_X'] = 97.02
-		sample['BEAM_CENTER_Y'] = 97.02
-		sample['WAVELENGTH'] = wavelength
-
-		picklePixels.reshape(grid(width, height))
-
+ 		sample['ACTIVE_AREAS'] = scitbx_array_family_flex_ext.int([0, 0, width, height])
+ 		sample['PIXEL_SIZE'] = pixelSize
+ 		sample['BEAM_CENTER_X'] = width / 2 * pixelSize
+ 		sample['BEAM_CENTER_Y'] = height / 2 * pixelSize
+ 		sample['WAVELENGTH'] = wavelength
+# 
+ 		picklePixels.reshape(grid(width, height))
+# 
 		sample['DATA'] = picklePixels
 
 		pickleName = identifier + '.pickle'
