00cc261f836cb2e1663b49683f4e4916ca707ac4
ginn <helenginn@Helen'sMacBookPro>
2017-02-14 23:27:50 +0000

    Only refine tilt Z if LUCA - again

--- source/GeometryRefiner.cpp
+++ source/GeometryRefiner.cpp
@@ -244,7 +244,11 @@ void GeometryRefiner::refineDetector(DetectorPtr detector, GeometryScoreType typ
         strategy->addParameter(&*detector, Detector::getNudgeTiltX, Detector::setNudgeTiltX, 0.001, 0.00001, "nudge_tx");
         strategy->addParameter(&*detector, Detector::getNudgeTiltY, Detector::setNudgeTiltY, 0.001, 0.00001, "nudge_ty");
         strategy->addParameter(&*detector, Detector::getNudgeZ, Detector::setNudgeZ, zNudge, 0.001, "nudge_z");
-        strategy->addParameter(&*detector, Detector::getGamma, Detector::setGamma, 0.001, 0.000001, "tilt_z");
+        
+        if (!detector->isLUCA())
+        {
+            strategy->addParameter(&*detector, Detector::getGamma, Detector::setGamma, 0.001, 0.000001, "tilt_z");
+        }
     }
     else if (type == GeometryScoreTypeInterpanel)
     {
