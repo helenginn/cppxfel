5d6e133c62b222ee46c8e6236c2f506439f20042
ginn <helenginn@Helen'sMacBookPro>
2017-02-20 16:03:10 +0000

    Do NOT refine pokeZ - it is too highly correlated with the other parameters

--- source/GeometryRefiner.cpp
+++ source/GeometryRefiner.cpp
@@ -351,7 +351,7 @@ void GeometryRefiner::refineDetector(DetectorPtr detector, GeometryScoreType typ
         detector->resetPoke();
         strategy->addParameter(&*detector, Detector::getPokeX, Detector::setPokeX, 0.1, 0.01, "poke_x");
         strategy->addParameter(&*detector, Detector::getPokeY, Detector::setPokeY, 0.1, 0.01, "poke_y");
-        strategy->addParameter(&*detector, Detector::getPokeZ, Detector::setPokeZ, zTiltNudge, 0.000001, "poke_z");
+ //       strategy->addParameter(&*detector, Detector::getPokeZ, Detector::setPokeZ, zTiltNudge, 0.000001, "poke_z");
     }
     else if (type == GeometryScoreTypeBeamCentre)
     {
