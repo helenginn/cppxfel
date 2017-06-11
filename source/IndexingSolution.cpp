5a5ef2c7ca49fdb8dd6354b1db34072ead39a45f
ginn <helenginn@Helen'sMacBookPro>
2017-06-11 13:53:50 +0100

    Don't know if this is necessary. Revert "Add removal of spot vectors which lead to bad solutions"
    This reverts commit 91da6f03eafd89790c640ddb1d565a523479b1ac.

--- source/IndexingSolution.cpp
+++ source/IndexingSolution.cpp
@@ -355,7 +355,7 @@ void IndexingSolution::pruneSpotVectors(std::vector<SpotVectorPtr> *spotVectors)
 }
 
 void IndexingSolution::removeSpotVectors(std::vector<SpotVectorPtr> *spotVectors)
-{
+{/*
     int count = 0;
     
     for (int i = 0; i < spotVectors->size(); i++)
@@ -371,7 +371,7 @@ void IndexingSolution::removeSpotVectors(std::vector<SpotVectorPtr> *spotVectors
     }
     
     logged << "Removed " << count << " spot vectors which led to a bad solution." << std::endl;
-    sendLog();
+    sendLog();*/
 }
 
 bool IndexingSolution::spotsAreNotTooClose(SpotVectorPtr observedVector)
